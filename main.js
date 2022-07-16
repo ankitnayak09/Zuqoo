506dc53ee2a1a8ea3574e95778c77706c90487db
suraj <surajarya745@gmail.com>
2022-07-17 01:24:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   $("#main_app").load("./html/navbar.html");
+// });
+
+// $("#cdn_link").load("/html/link.html");
+
+$("#desk_nav").load("/html/navbar.html");
+
+$("#footer").load("/html/footer.html");
+// $(".menu").click(function () {
+//   $("#mobile_navbar_container").fadeToggle();
+// });
---
+++ public/javascript/navbar.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+  $("#main_nav").load("navbar.html");
+});
