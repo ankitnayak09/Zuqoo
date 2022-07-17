3dc6b9b1d44684e7a3c813b4901dccec331506a9
Ankit Kumar Nayak <ankitnayak882@gmail.com>
2022-07-18 00:24:02 +0530

    Footer Changed to Class

--- main.js
+++ main.js
@@ -4,9 +4,26 @@
 
 // $("#cdn_link").load("/html/link.html");
 
-$("#desk_nav").load("/html/navbar.html");
+$(".desk_nav").load("/html/navbar.html");
 
-$("#footer").load("/html/footer.html");
+$(".footer").load("/html/footer.html");
 // $(".menu").click(function () {
 //   $("#mobile_navbar_container").fadeToggle();
 // });
+
+$('#product-container .col:nth-of-type(2) .row .col').click(function () {
+    $("#product-container .col:nth-of-type(2) .row .col").each(function() {
+        $(this).removeClass('active');
+    })
+    $(this).addClass('active');
+    
+    $("#product-container .col:nth-of-type(3) .row").each(function(el) {
+        if($(el).attr('id') != $(this).attr("data-category")){
+            $(el).removeClass('active');
+        }
+        else {
+            $(el).addClass('active');
+        }
+    })
+
+})
